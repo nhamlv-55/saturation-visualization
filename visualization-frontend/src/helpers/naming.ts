34dfab1cc5e0979cf89fd40c8e8b4929cb61cbac
Aishwarya Ramanathan <cutieice@gmail.com>
2020-10-23 17:42:32 -0400

    Updates

--- visualization-frontend/src/helpers/naming.ts
+++
@@ -1,9 +0,0 @@
-export function potholeToNormal(input) {
-    let words = input.split("_");
-    for (let i = 0; i < words.length; i++){
-        words[i] = words[i][0].toUpperCase() + words[i].substring(1);
-    }
-    
-    return words.join(" ");
-    
-}
