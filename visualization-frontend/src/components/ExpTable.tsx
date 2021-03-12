5e23ac3f6e7f8002d1c961463ba1ef12e55d0a5d
Nham Le <nv3le@uwaterloo.ca>
2021-03-11 22:09:52 -0500

    add delete_exp

--- visualization-frontend/src/components/ExpTable.tsx
+++ visualization-frontend/src/components/ExpTable.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import '../styles/NodeMenu.css';
+import '../styles/ExpTable.css';
 import {Link} from 'react-router-dom';
 
 type Props = {
@@ -25,14 +25,45 @@ export default class ExpTable extends React.Component<Props, State> {
 
     render() {
         return (
-                <div>
-                {this.state.exps.map((item, index) => (
-                        <h5 key = {item.exp_name}><Link to={{pathname: `/replay/${item.exp_name}`}} >{item.exp_name}</Link> ${item.done}</h5>
+                <div className="exp-table">
+                    {this.state.exps.map((item, index) => (
+                        <div className="exp" key = {item.exp_name}>
+                            <h5><Link to={{pathname: `/replay/${item.exp_name}`}} >{item.exp_name}</Link></h5>
+                            <button onClick={this.deleteExp.bind(this, item.exp_name)}>X</button>
+                        </div>
                 ))}
                 <p>{this.state.isFetching ? 'Fetching experiments...' : ''}</p>
                 </div>
         )
     }
+    async deleteExp(expName: string) {
+        this.setState({
+            isFetching: true,
+        });
+
+        const fetchedJSON = await fetch('http://localhost:5000/spacer/delete_exp', {
+            method: 'POST',
+            mode: 'cors',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }, body : JSON.stringify({"expName": expName})
+        });
+
+        try {
+            const json = await fetchedJSON.json();
+            console.log(json)
+            this.setState({isFetching: false, exps: json.exps_list})
+        } catch (error) {
+            if (error.name === "SatVisAssertionError") {
+                throw error;
+            }
+            this.setState({
+                exps: []
+            });
+        }
+    }
+
     async fetchExps() {
         this.setState({
             isFetching: true,
